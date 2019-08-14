8d3b00ae0407da19f6acaacd5b1ec1155edf5f24
LokiVKlokeNaAndoke <ma.ba1@rambler.ru>
2019-08-14 16:17:26 +0300

    preview works

--- app/src/main/java/com/neuralcamera/MainActivity.kt
+++ app/src/main/java/com/neuralcamera/MainActivity.kt
@@ -1,17 +1,82 @@
 package com.neuralcamera
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import co.infinum.goldeneye.GoldenEye
+import co.infinum.goldeneye.InitCallback
+import co.infinum.goldeneye.PictureCallback
+import co.infinum.goldeneye.models.Facing
+import co.infinum.goldeneye.models.FlashMode
+import co.infinum.goldeneye.models.Size
 import kotlinx.android.synthetic.main.activity_main.*
 
+
 class MainActivity : AppCompatActivity() {
+    private var goldenEye: GoldenEye? = null
+
+    fun takePhoto(@Suppress("UNUSED_PARAMETER") view: View) {
+        goldenEye?.takePicture(object : PictureCallback() {
+            override fun onError(t: Throwable) {
+                println(t.message)
+            }
+
+            override fun onPictureTaken(picture: Bitmap) {
+
+            }
+        })
+    }
+
+    private fun requestAllPermissions() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), 0)
+        }
+    }
+
+    private fun openCamera() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            /* Find back camera */
+            val backCamera = goldenEye?.availableCameras?.find { it.facing == Facing.BACK }
+            /* Open back camera */
+            if (backCamera != null) {
+                goldenEye?.open(camera_preview, backCamera, object : InitCallback() {
+                    override fun onError(t: Throwable) {
+                        println("Error: ${t.message}")
+                    }
+                })
+                println(goldenEye?.config?.supportedPictureSizes)
+                println(goldenEye?.config?.supportedPreviewSizes)
+            } else {
+                println("Error: Couldn't find camera")
+            }
+        } else {
+            requestAllPermissions()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //android.hardware.camera2.
+        requestAllPermissions()
+
+        goldenEye = GoldenEye.Builder(this).build()
     }
 
+    override fun onResume() {
+        super.onResume()
+        openCamera()
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        goldenEye?.release()
+    }
 
 
     /**
